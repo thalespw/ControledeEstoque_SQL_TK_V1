8f369119e2623c84ba128162b437c3d45bef320a
thalespw <thalesmln@gmail.com>
2025-11-03 09:58:06 -0300

    Separação frontend e backend

--- main.py
+++ main.py
@@ -10,16 +10,21 @@ conn = sqlite3.connect('Estoque.db')
 cursor = conn.cursor()
 
 
+from main_window_tk import criar_interface
+window,canvas,background_img ,nome_insumo, lote_insumo, data_insumo, qtde_insumo, b_search,b_delete,b_edit,b_add,b_save_add,b_save_edit,img0,img1,img2,img3,img4,img5 = criar_interface()
 
 
 def limpar_tela():
+    habilitar_campos()
     lote_insumo.delete(0,END)
     data_insumo.delete(0,END)
     qtde_insumo.delete(0,END)
     nome_insumo.delete(0,END)
+    desabilitar_campos()
 
 def carregar_ultimo():
     limpar_tela()
+    habilitar_campos()
     cursor.execute("SELECT Produto, Quantidade, DataValidade, Lote FROM Estoque ORDER BY id DESC LIMIT 1")
     ultimo = cursor.fetchone()
 
@@ -28,6 +33,7 @@ def carregar_ultimo():
         lote_insumo.insert(0, ultimo[3])
         qtde_insumo.insert(0, str(ultimo[1]))
         data_insumo.set_date(ultimo[2])
+    desabilitar_campos()
 
 def adicionar_insumo():
     limpar_tela()
@@ -51,7 +57,7 @@ def deletar_insumo():
     nome_insumo.delete(0,END)
 
 def editar():
-    b5.config(state="normal")
+    pass
 
 def salvar_edit():
     produto_original = nome_insumo.get()
@@ -126,191 +132,47 @@ def salvar_add():
     ("{nome_insumo.get()}", {qtde_insumo.get()}, "{data_insumo.get()}", {lote_insumo.get()})
     """)
     conn.commit()
-    caixa_texto.delete("1.0", END)
     
     messagebox.showinfo("ADICIONAR",f"{nome_insumo.get()} adicionado com sucesso!")
-    b4.config(state="disable")
+    esconder_botao_salvar_add()
     limpar_tela()
 
 #########BOTOES SALVAR - MOSTRAR / ESCONDER    
 def mostrar_botao_salvar_add():
-    b4.place(x=69, y=550, width=40, height=46)
+    b_save_add.place(x=69, y=550, width=40, height=46)
+    nome_insumo.focus()
 
 def esconder_botao_salvar_add():
-    b4.place_forget()
+    b_save_add.place_forget()
 
 def mostrar_botao_salvar_edit():
-    b5.place(x=69, y=550, width=40, height=46)
+    b_save_edit.place(x=69, y=550, width=40, height=46)
 
 def esconder_botao_salvar_edit():
-    b5.place_forget()
+    b_save_edit.place_forget()
+######### HABILITAR/DESABILITAR CAMPOS PARA EDIÇÃO #######
+
+def habilitar_campos():
+    nome_insumo.config(state="normal")
+    lote_insumo.config(state="normal")
+    data_insumo.config(state="normal")
+    qtde_insumo.config(state="normal")
+    nome_insumo.focus()
+
+def desabilitar_campos():
+    lote_insumo.config(state="disabled",disabledforeground="black")
+    nome_insumo.config(state="disabled",disabledforeground="black")
+    data_insumo.config(state="disabled",disabledforeground="black")
+    qtde_insumo.config(state="disabled",disabledforeground="black")
+    nome_insumo.focus()
+
+b_add.config(command=adicionar_insumo)
+b_edit.config(command=editar)
+b_search.config(command=abrir_janela_pesquisa)
+b_delete.config(command=deletar_insumo)
+b_save_add.config(command=salvar_add)
+b_save_edit.config(command=salvar_edit)
     
-######### criação da Janela ##################
-    
-window = Tk()
-#window.iconbitmap(r"C:\net\hashtag\ControledeEstoque_SQL_TK_V1\janela\mann.ico")
-window.geometry("711x646")
-window.configure(bg = "#ffffff")
-canvas = Canvas(
-    window,
-    bg = "#ffffff",
-    height = 646,
-    width = 711,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge")
-canvas.place(x = 0, y = 0)
-
-background_img = PhotoImage(file = f"janela/background.png")
-background = canvas.create_image(
-    355.5, 323.0,
-    image=background_img)
-
-img0 = PhotoImage(file = f"janela/imgbg_search.png")
-b0 = Button(
-    image = img0,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = abrir_janela_pesquisa,
-    relief = "flat")
-
-b0.place(
-    x = 64, y = 326,
-    width = 50,
-    height = 53)
-
-img1 = PhotoImage(file = f"janela/imgbg_delete.png")
-b1 = Button(
-    image = img1,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = deletar_insumo,
-    relief = "flat")
-
-b1.place(
-    x = 69, y = 397,
-    width = 40,
-    height = 46)
-
-img2 = PhotoImage(file = f"janela/imgbg_edit.png")
-b2 = Button(
-    image = img2,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = editar,
-    relief = "flat")
-
-b2.place(
-    x = 66, y = 258,
-    width = 50,
-    height = 50)
-
-img3 = PhotoImage(file = f"janela/imgbg_add.png")
-b3 = Button(
-    image = img3,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = adicionar_insumo,
-    relief = "flat")
-
-b3.place(
-    x = 64, y = 190,
-    width = 49,
-    height = 53)
-
-img4 = PhotoImage(file = f"janela/imgcheck.png")
-b4 = Button(
-    image = img4,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = salvar_add,
-    relief = "flat")
-
-
-
-img5 = PhotoImage(file = f"janela/imgcheck.png")
-b5 = Button(
-    image = img5,
-    borderwidth = 0,
-    highlightthickness = 0,
-    command = salvar_edit,
-    relief = "flat")
-
-
-entry0_img = PhotoImage(file = f"janela/img_textBox0.png")
-entry0_bg = canvas.create_image(
-    455.0, 560.0,
-    image = entry0_img)
-
-caixa_texto = Text(
-    bd = 0,
-    bg = "#ffffff",
-    highlightthickness = 0)
-
-caixa_texto.place(
-    x = 250, y = 502,
-    width = 410,
-    height = 114)
-
-entry1_img = PhotoImage(file = f"janela/img_textBox1.png")
-entry1_bg = canvas.create_image(
-    517.0, 294.5,
-    image = entry1_img)
-
-nome_insumo = Entry(
-    bd = 0,
-    bg = "#ffffff",
-    highlightthickness = 0)
-
-nome_insumo.place(
-    x = 377, y = 278,
-    width = 280,
-    height = 31)
-
-entry2_img = PhotoImage(file = f"janela/img_textBox2.png")
-entry2_bg = canvas.create_image(
-    517.0, 340.5,
-    image = entry2_img)
-
-data_insumo = DateEntry(
-    bd = 0,
-    bg = "#ffffff",
-    highlightthickness = 0)
-
-data_insumo.place(
-    x = 377, y = 324,
-    width = 280,
-    height = 31)
-
-entry3_img = PhotoImage(file = f"janela/img_textBox3.png")
-entry3_bg = canvas.create_image(
-    517.0, 388.5,
-    image = entry3_img)
-
-lote_insumo = Entry(
-    bd = 0,
-    bg = "#ffffff",
-    highlightthickness = 0)
-
-lote_insumo.place(
-    x = 377, y = 372,
-    width = 280,
-    height = 31)
-
-entry4_img = PhotoImage(file = f"janela/img_textBox4.png")
-entry4_bg = canvas.create_image(
-    517.0, 436.5,
-    image = entry4_img)
-
-qtde_insumo = Entry(
-    bd = 0,
-    bg = "#ffffff",
-    highlightthickness = 0)
-
-qtde_insumo.place(
-    x = 377, y = 420,
-    width = 280,
-    height = 31)
 
 
 
---
+++ main_window_tk.py
@@ -0,0 +1,170 @@
+from tkinter import *
+from tkinter import messagebox
+from tkcalendar import DateEntry
+
+def criar_interface():
+    window = Tk()
+    #window.iconbitmap(r"C:\net\hashtag\ControledeEstoque_SQL_TK_V1\janela\mann.ico")
+    window.geometry("711x646")
+    window.configure(bg = "#ffffff")
+    canvas = Canvas(
+        window,
+        bg = "#ffffff",
+        height = 646,
+        width = 711,
+        bd = 0,
+        highlightthickness = 0,
+        relief = "ridge")
+    canvas.place(x = 0, y = 0)
+
+    background_img = PhotoImage(file = f"janela/background.png")
+    background = canvas.create_image(
+        355.5, 323.0,
+        image=background_img)
+
+    img0 = PhotoImage(file = f"janela/imgbg_search.png")
+    b_search = Button(
+        image = img0,
+        borderwidth = 0,
+        highlightthickness = 0,
+        relief = "flat")
+
+    b_search.place(
+        x = 64, y = 326,
+        width = 50,
+        height = 53)
+
+    img1 = PhotoImage(file = f"janela/imgbg_delete.png")
+    b_delete = Button(
+        image = img1,
+        borderwidth = 0,
+        highlightthickness = 0,
+        relief = "flat")
+
+    b_delete.place(
+        x = 69, y = 397,
+        width = 40,
+        height = 46)
+
+    img2 = PhotoImage(file = f"janela/imgbg_edit.png")
+    b_edit = Button(
+        image = img2,
+        borderwidth = 0,
+        highlightthickness = 0,
+        relief = "flat")
+
+    b_edit.place(
+        x = 66, y = 258,
+        width = 50,
+        height = 50)
+
+    img3 = PhotoImage(file = f"janela/imgbg_add.png")
+    b_add = Button(
+        image = img3,
+        borderwidth = 0,
+        highlightthickness = 0,
+        relief = "flat")
+
+    b_add.place(
+        x = 64, y = 190,
+        width = 49,
+        height = 53)
+
+    img4 = PhotoImage(file = f"janela/imgcheck.png")
+    b_save_add = Button(
+        image = img4,
+        borderwidth = 0,
+        highlightthickness = 0,
+        relief = "flat")
+
+
+
+    img5 = PhotoImage(file = f"janela/imgcheck.png")
+    b_save_edit = Button(
+        image = img5,
+        borderwidth = 0,
+        highlightthickness = 0,
+        relief = "flat")
+
+    ###### CAIXAS DE TEXTO #############
+
+    entry0_img = PhotoImage(file = f"janela/img_textBox0.png")
+    entry0_bg = canvas.create_image(
+        455.0, 560.0,
+        image = entry0_img)
+
+    caixa_texto = Text(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0)
+
+    caixa_texto.place(
+        x = 250, y = 502,
+        width = 410,
+        height = 114)
+
+    entry1_img = PhotoImage(file = f"janela/img_textBox1.png")
+    entry1_bg = canvas.create_image(
+        517.0, 294.5,
+        image = entry1_img)
+
+    nome_insumo = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0,
+        state="disabled",disabledforeground="black")
+
+    nome_insumo.place(
+        x = 377, y = 278,
+        width = 280,
+        height = 31)
+
+    entry2_img = PhotoImage(file = f"janela/img_textBox2.png")
+    entry2_bg = canvas.create_image(
+        517.0, 340.5,
+        image = entry2_img)
+
+    data_insumo = DateEntry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0,
+        state="disabled",disabledforeground="black")
+
+    data_insumo.place(
+        x = 377, y = 324,
+        width = 280,
+        height = 31)
+
+    entry3_img = PhotoImage(file = f"janela/img_textBox3.png")
+    entry3_bg = canvas.create_image(
+        517.0, 388.5,
+        image = entry3_img)
+
+    lote_insumo = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0,
+        state="disabled",disabledforeground="black")
+
+    lote_insumo.place(
+        x = 377, y = 372,
+        width = 280,
+        height = 31)
+
+    entry4_img = PhotoImage(file = f"janela/img_textBox4.png")
+    entry4_bg = canvas.create_image(
+        517.0, 436.5,
+        image = entry4_img)
+
+    qtde_insumo = Entry(
+        bd = 0,
+        bg = "#ffffff",
+        highlightthickness = 0,
+        state="disabled",disabledforeground="black")
+
+    qtde_insumo.place(
+        x = 377, y = 420,
+        width = 280,
+        height = 31)
+
+    return window,canvas,background_img ,nome_insumo, lote_insumo, data_insumo, qtde_insumo, b_search,b_delete,b_edit,b_add,b_save_add,b_save_edit,img0,img1,img2,img3,img4,img5
